d3a35a6afdb362c841dfd7e1e7846d1f0ba23d34
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-09 22:43:36 +0200

    slightly change date format

--- AWS/Query.hs
+++ AWS/Query.hs
@@ -121,7 +121,7 @@ fmtTime :: String -> UTCTime -> String
 fmtTime = formatTime defaultTimeLocale
 
 fmtRfc822Time :: UTCTime -> String
-fmtRfc822Time = fmtTime rfc822DateFormat
+fmtRfc822Time = fmtTime "%a, %_d %b %Y %H:%M:%S GMT"
 
 fmtAmzTime :: UTCTime -> String
 fmtAmzTime = fmtTime "%Y-%m-%dT%H:%M:%S"
