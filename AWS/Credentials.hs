1a9dc0eabf8163acfeb9961236927a5fc69432ed
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-11 18:27:30 +0200

    add AbsoluteTimeInfo and (relative) TimeInfo and fix missing imports in AWS.Credentials (and remove redundant ones in AWS.Query)

--- AWS/Credentials.hs
+++ AWS/Credentials.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module AWS.Credentials
 where
   
-import AWS.Query
-import AWS.Util
-  
+import           AWS.Query
+import           AWS.Util
+import           Control.Applicative
+import           Data.Function
+import           Data.List
+import           Data.Time
+import qualified Network.HTTP             as HTTP
+import qualified Codec.Binary.UTF8.String as Utf8
+import           Data.HMAC
+import qualified Codec.Binary.Base64      as Base64
+
 data Credentials
     = Credentials {
         accessKeyID :: String
@@ -11,26 +21,38 @@ data Credentials
       }
     deriving (Show)
 
-data TimeInfo
-    = Timestamp { fromTimestamp :: UTCTime }
-    | Expires { fromExpires :: UTCTime }
+data AbsoluteTimeInfo
+    = AbsoluteTimestamp { fromAbsoluteTimestamp :: UTCTime }
+    | AbsoluteExpires { fromAbsoluteExpires :: UTCTime }
     deriving (Show)
 
-addTimeInfo :: TimeInfo -> Query -> Query
-addTimeInfo (Timestamp time) q@Query{..} = q {
-                                             query = ("Timestamp", fmtAmzTime time) : query
-                                           , date = Just time
-                                           }
-addTimeInfo (Expires time) q@Query{..} = q {
-                                           query = ("Expires", fmtAmzTime time) : query
-                                         }
+data TimeInfo
+    = Timestamp
+    | ExpiresAt { fromExpiresAt :: UTCTime }
+    | ExpiresIn { fromExpiresIn :: NominalDiffTime }
+    deriving (Show)
+      
+makeAbsoluteTimeInfo :: TimeInfo -> IO AbsoluteTimeInfo
+makeAbsoluteTimeInfo Timestamp     = AbsoluteTimestamp <$> getCurrentTime
+makeAbsoluteTimeInfo (ExpiresAt t) = return $ AbsoluteExpires t
+makeAbsoluteTimeInfo (ExpiresIn s) = AbsoluteExpires . addUTCTime s <$> getCurrentTime
+
+addTimeInfo :: AbsoluteTimeInfo -> Query -> Query
+addTimeInfo (AbsoluteTimestamp time) q@Query{..} = q {
+                                                     query = ("Timestamp", fmtAmzTime time) : query
+                                                   , date = Just time
+                                                   }
+addTimeInfo (AbsoluteExpires time) q@Query{..} = q {
+                                                   query = ("Expires", fmtAmzTime time) : query
+                                                 }
 
 addSignatureData :: Credentials -> Query -> Query
 addSignatureData Credentials{..} q@Query{..} = q {
                                            query = [("AWSAccessKeyId", accessKeyID), ("SignatureMethod", "HmacSHA1"), ("SignatureVersion", "2")]
                                                    ++ query
                                          }
-                                               
+
+stringToSign :: Query -> String
 stringToSign Query{..} = case api of 
                            SimpleDB -> show method ++ "\n" ++
                                        host ++ "\n" ++
@@ -42,5 +64,5 @@ signPreparedQuery :: Credentials -> Query -> Query
 signPreparedQuery Credentials{..} q@Query{..} = q { query = ("Signature", sig) : query }
     where sig = Base64.encode $ hmac_sha1 (Utf8.encode secretAccessKey) (Utf8.encode . stringToSign $ q)
                            
-signQuery :: TimeInfo -> Credentials -> Query -> Query
+signQuery :: AbsoluteTimeInfo -> Credentials -> Query -> Query
 signQuery ti cr = signPreparedQuery cr . addSignatureData cr . addTimeInfo ti
--- AWS/Query.hs
+++ AWS/Query.hs
@@ -4,18 +4,12 @@ module AWS.Query
 where
 
 import           AWS.Http
-import           AWS.Util
 import           Control.Arrow
 import           Control.Monad
-import           Data.Function
-import           Data.List
 import           Data.Time
 import qualified Network.HTTP               as HTTP
-import           Network.URI                hiding (query)
+import           Network.URI                (nullURI, URI(..), URIAuth(..))
 import qualified Data.ByteString.Lazy.Char8 as L
-import           Data.HMAC
-import qualified Codec.Binary.Base64        as Base64
-import qualified Codec.Binary.UTF8.String   as Utf8
 
 class AsQuery d i | d -> i where
     asQuery :: i -> d -> Query
@@ -52,11 +46,12 @@ addQueryMaybe :: (a -> String) -> (String, Maybe a) -> Query -> Query
 addQueryMaybe f (name, Just a) q = q { query = (name, f a) : query q }
 addQueryMaybe _ (_, Nothing) q = q
 
+dot :: String -> String -> String
 dot x y = x ++ '.' : y
 
 queryList :: (a -> [(String, String)]) -> String -> [a] -> [(String, String)]
 queryList f prefix xs = concat $ zipWith combine prefixList (map f xs)
-    where prefixList = map (dot prefix . show) [1..]
+    where prefixList = map (dot prefix . show) [(1 :: Int) ..]
           combine pf = map $ first (pf `dot`)
           
 addQueryList :: (a -> [(String, String)]) -> String -> [a] -> Query -> Query 
