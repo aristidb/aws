91cb5be5162cbf9275e004e0e387b7039249ebc2
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-12 18:54:11 +0200

    generalise orElse to all monads

--- AWS/Util.hs
+++ AWS/Util.hs
@@ -14,7 +14,5 @@ fmtRfc822Time = fmtTime "%a, %_d %b %Y %H:%M:%S GMT"
 fmtAmzTime :: UTCTime -> String
 fmtAmzTime = fmtTime "%Y-%m-%dT%H:%M:%S"
 
-orElse :: IO (Maybe a) -> IO (Maybe a) -> IO (Maybe a)
-orElse a b = do
-  x <- a
-  if (isNothing x) then b else return x
+orElse :: Monad m => m (Maybe a) -> m (Maybe a) -> m (Maybe a)
+orElse a b = a >>= \x -> if (isNothing x) then b else return x
