c09f36d5d9b2f322d27ea52a2542fa07564c8111
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-23 22:28:08 +0200

    move some SimpleDb model types into AWS.SimpleDb.Model

--- AWS/SimpleDb.hs
+++ AWS/SimpleDb.hs
@@ -5,6 +5,7 @@ where
   
 import           AWS.SimpleDb.Info
 import           AWS.SimpleDb.Error
+import           AWS.SimpleDb.Model
 import           AWS.Query
 import           AWS.Http
 import           AWS.Response
@@ -171,13 +172,6 @@ instance SdbFromResponse DomainMetadataResponse where
 
 instance Transaction DomainMetadata SdbInfo (SdbResponse DomainMetadataResponse) Error
 
-data Attribute a
-    = ForAttribute { attributeName :: String, attributeData :: a }
-    deriving (Show)
-             
-attributeQuery :: (a -> [(String, String)]) -> Attribute a -> [(String, String)]
-attributeQuery  f (ForAttribute name x) =  ("Name", name) : f x
-
 data GetAttributes
     = GetAttributes {
         gaItemName :: String
@@ -249,35 +243,6 @@ instance SdbFromResponse PutAttributesResponse where
 
 instance Transaction PutAttributes SdbInfo (SdbResponse PutAttributesResponse) Error
 
-data SetAttribute
-    = SetAttribute { setAttribute :: String, isReplaceAttribute :: Bool }
-    deriving (Show)
-             
-addAttribute :: String -> String -> Attribute SetAttribute
-addAttribute name value = ForAttribute name (SetAttribute value False)
-
-replaceAttribute :: String -> String -> Attribute SetAttribute
-replaceAttribute name value = ForAttribute name (SetAttribute value True)
-             
-setAttributeQuery :: SetAttribute -> [(String, String)]
-setAttributeQuery (SetAttribute value replace)
-    = ("Value", value) : (if replace then [("Replace", awsTrue)] else [])
-             
-data ExpectedAttribute
-    = ExpectedValue { expectedAttributeValue :: String }
-    | ExpectedExists { expectedAttributeExists :: Bool }
-    deriving (Show)
-             
-expectedValue :: String -> String -> Attribute ExpectedAttribute
-expectedValue name value = ForAttribute name (ExpectedValue value)
-
-expectedExists :: String -> Bool -> Attribute ExpectedAttribute
-expectedExists name exists = ForAttribute name (ExpectedExists exists)
-             
-expectedAttributeQuery :: ExpectedAttribute -> [(String, String)]
-expectedAttributeQuery (ExpectedValue value) = [("Value", value)]
-expectedAttributeQuery (ExpectedExists exists) = [("Exists", awsBool exists)]
-
 data BatchPutAttributes
     = BatchPutAttributes {
         bpaItems :: [Item [Attribute SetAttribute]]
@@ -302,13 +267,6 @@ instance SdbFromResponse BatchPutAttributesResponse where
     sdbFromResponse = BatchPutAttributesResponse <$ testElementNameUI "BatchPutAttributesResponse"
 
 instance Transaction BatchPutAttributes SdbInfo (SdbResponse BatchPutAttributesResponse) Error
-
-data Item a
-    = Item { itemName :: String, itemData :: a }
-    deriving (Show)
-             
-itemQuery :: (a -> [(String, String)]) -> Item a -> [(String, String)]
-itemQuery f (Item name x) = ("ItemName", name) : f x
              
 data Select
     = Select {
---
+++ AWS/SimpleDb/Model.hs
@@ -0,0 +1,47 @@
+module AWS.SimpleDb.Model
+where
+  
+import AWS.Query
+
+data Attribute a
+    = ForAttribute { attributeName :: String, attributeData :: a }
+    deriving (Show)
+             
+data SetAttribute
+    = SetAttribute { setAttribute :: String, isReplaceAttribute :: Bool }
+    deriving (Show)
+
+attributeQuery :: (a -> [(String, String)]) -> Attribute a -> [(String, String)]
+attributeQuery  f (ForAttribute name x) =  ("Name", name) : f x
+             
+addAttribute :: String -> String -> Attribute SetAttribute
+addAttribute name value = ForAttribute name (SetAttribute value False)
+
+replaceAttribute :: String -> String -> Attribute SetAttribute
+replaceAttribute name value = ForAttribute name (SetAttribute value True)
+             
+setAttributeQuery :: SetAttribute -> [(String, String)]
+setAttributeQuery (SetAttribute value replace)
+    = ("Value", value) : (if replace then [("Replace", awsTrue)] else [])
+             
+data ExpectedAttribute
+    = ExpectedValue { expectedAttributeValue :: String }
+    | ExpectedExists { expectedAttributeExists :: Bool }
+    deriving (Show)
+             
+expectedValue :: String -> String -> Attribute ExpectedAttribute
+expectedValue name value = ForAttribute name (ExpectedValue value)
+
+expectedExists :: String -> Bool -> Attribute ExpectedAttribute
+expectedExists name exists = ForAttribute name (ExpectedExists exists)
+             
+expectedAttributeQuery :: ExpectedAttribute -> [(String, String)]
+expectedAttributeQuery (ExpectedValue value) = [("Value", value)]
+expectedAttributeQuery (ExpectedExists exists) = [("Exists", awsBool exists)]
+
+data Item a
+    = Item { itemName :: String, itemData :: a }
+    deriving (Show)
+             
+itemQuery :: (a -> [(String, String)]) -> Item a -> [(String, String)]
+itemQuery f (Item name x) = ("ItemName", name) : f x
