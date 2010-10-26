e322a08da9f90ad8c3ab799b7d3b82ac270b1a66
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-26 23:41:51 +0200

    split SimpleDb response into own module

--- Aws/SimpleDb.hs
+++ Aws/SimpleDb.hs
@@ -10,6 +10,7 @@ import           Aws.SimpleDb.Error
 import           Aws.SimpleDb.Info
 import           Aws.SimpleDb.Metadata
 import           Aws.SimpleDb.Model
+import           Aws.SimpleDb.Response
 import           Aws.Transaction
 import           Control.Applicative
 import           Data.Maybe
@@ -21,45 +22,6 @@ import           Text.XML.Monad
 import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Network.HTTP               as HTTP
 import qualified Text.XML.Light             as XL
-
-data SdbResponse a
-    = SdbResponse { 
-        fromSdbResponse :: a
-      , sdbResponseMetadata :: SdbMetadata
-      }
-    deriving (Show)
-
-instance Functor SdbResponse where
-    fmap f (SdbResponse a m) = SdbResponse (f a) m
-
-instance SdbFromResponse a => FromResponse (SdbResponse a) SdbError where
-    fromResponse = do
-          status <- asks (responseStatus . httpResponse)
-          parseXmlResponse >>> fromXml status
-        where fromXml :: SdbFromResponse a => Int -> Xml SdbError XL.Element (SdbResponse a)
-              fromXml status = do
-                     requestId <- strContent <<< findElementNameUI "RequestID"
-                     boxUsage <- tryMaybe $ strContent <<< findElementNameUI "BoxUsage"
-                     let metadata = SdbMetadata requestId boxUsage
-                     innerTry <- try $ fromXmlInner status
-                     inner <- case innerTry of
-                       Left err -> raise $ WithMetadata err metadata
-                       Right response -> return response
-                     return $ SdbResponse inner metadata
-              fromXmlInner :: SdbFromResponse a => Int -> Xml SdbError XL.Element a
-              fromXmlInner status = do
-                     xmlError <- tryMaybe $ findElementNameUI "Error"
-                     case xmlError of
-                       Just err -> mapply (fromError status) err
-                       Nothing -> sdbFromResponse
-              fromError :: Int -> Xml SdbError XL.Element a
-              fromError status = do
-                     errCode <- nameToErrorCode <$> strContent <<< findElementNameUI "Code"
-                     errMessage <- strContent <<< findElementNameUI "Message"
-                     raise $ SdbError status errCode errMessage
-
-class SdbFromResponse a where
-    sdbFromResponse :: Xml SdbError XL.Element a
 
 data CreateDomain
     = CreateDomain {
---
+++ Aws/SimpleDb/Response.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Aws.SimpleDb.Response
+where
+
+import           Aws.Http
+import           Aws.Response
+import           Aws.SimpleDb.Error
+import           Aws.SimpleDb.Metadata
+import           Control.Applicative
+import           MonadLib
+import           MonadLib.Compose
+import           Text.XML.Monad
+import qualified Text.XML.Light        as XL
+
+data SdbResponse a
+    = SdbResponse { 
+        fromSdbResponse :: a
+      , sdbResponseMetadata :: SdbMetadata
+      }
+    deriving (Show)
+
+instance Functor SdbResponse where
+    fmap f (SdbResponse a m) = SdbResponse (f a) m
+
+instance SdbFromResponse a => FromResponse (SdbResponse a) SdbError where
+    fromResponse = do
+          status <- asks (responseStatus . httpResponse)
+          parseXmlResponse >>> fromXml status
+        where fromXml :: SdbFromResponse a => Int -> Xml SdbError XL.Element (SdbResponse a)
+              fromXml status = do
+                     requestId <- strContent <<< findElementNameUI "RequestID"
+                     boxUsage <- tryMaybe $ strContent <<< findElementNameUI "BoxUsage"
+                     let metadata = SdbMetadata requestId boxUsage
+                     innerTry <- try $ fromXmlInner status
+                     inner <- case innerTry of
+                       Left err -> raise $ WithMetadata err metadata
+                       Right response -> return response
+                     return $ SdbResponse inner metadata
+              fromXmlInner :: SdbFromResponse a => Int -> Xml SdbError XL.Element a
+              fromXmlInner status = do
+                     xmlError <- tryMaybe $ findElementNameUI "Error"
+                     case xmlError of
+                       Just err -> mapply (fromError status) err
+                       Nothing -> sdbFromResponse
+              fromError :: Int -> Xml SdbError XL.Element a
+              fromError status = do
+                     errCode <- nameToErrorCode <$> strContent <<< findElementNameUI "Code"
+                     errMessage <- strContent <<< findElementNameUI "Message"
+                     raise $ SdbError status errCode errMessage
+
+class SdbFromResponse a where
+    sdbFromResponse :: Xml SdbError XL.Element a
