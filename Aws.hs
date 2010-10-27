fb202757f3ea8c86c3298ee5bdbfa983c0a307c0
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-27 14:34:33 +0200

    add uber-module "Aws"

---
+++ Aws.hs
@@ -0,0 +1,21 @@
+module Aws
+(
+  module Aws.Aws
+, module Aws.Credentials
+, module Aws.Http
+, module Aws.Query
+, module Aws.Response
+, module Aws.SimpleDb
+, module Aws.Transaction
+, module Aws.Util
+)
+where
+
+import Aws.Aws
+import Aws.Credentials
+import Aws.Http
+import Aws.Query
+import Aws.Response
+import Aws.SimpleDb
+import Aws.Transaction
+import Aws.Util
