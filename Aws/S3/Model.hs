21f970ace4410ec0426a2ba404f2ad4de16fa553
Aristid Breitkreuz <aristidb@googlemail.com>
2011-01-28 21:20:49 +0100

    moar S3 model

--- Aws/S3/Model.hs
+++ Aws/S3/Model.hs
@@ -1,26 +1,32 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Aws.S3.Model
 where
 
 import           Data.ByteString.Char8 ({- IsString -})
 import           Data.Time
-import qualified Data.ByteString       as B
 
-type CanonicalUserId = B.ByteString
+type CanonicalUserId = String
 
-type Bucket = B.ByteString
+data UserInfo
+    = UserInfo {
+        userId :: CanonicalUserId
+      , userDisplayName :: String
+      }
+    deriving (Show)
+
+type Bucket = String
 
 data BucketInfo
     = BucketInfo {
         bucketName :: Bucket
       , bucketCreationDate :: UTCTime
       }
+    deriving (Show)
 
 data LocationConstraint 
     = EU | UsWest1 | ApSouthEast1 | UsClassic
     deriving (Show)
 
-locationConstraintToId :: LocationConstraint -> B.ByteString
+locationConstraintToId :: LocationConstraint -> String
 locationConstraintToId lc
     = case lc of
         EU -> "EU"
@@ -28,7 +34,7 @@ locationConstraintToId lc
         ApSouthEast1 -> "ap-southeast-1"
         UsClassic -> ""
 
-idToLocationConstraint :: B.ByteString -> Maybe LocationConstraint
+idToLocationConstraint :: String -> Maybe LocationConstraint
 idToLocationConstraint i    
     = case i of
         "EU" -> Just EU
