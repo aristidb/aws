cac577d52035f37172005db69a46b20d41f18ff8
Aristid Breitkreuz <aristidb@googlemail.com>
2011-02-12 22:59:56 +0100

    stub for s3SignQuery

--- Aws/S3/Query.hs
+++ Aws/S3/Query.hs
@@ -1,16 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Aws.S3.Query
 where
-  
-{-
-        S3 -> B.intercalate "\n" $ concat [[httpMethod method]
-                                          , [fromMaybe "" contentMd5]
-                                          , [fromMaybe "" contentType]
-                                          , [case ti of
-                                               AbsoluteTimestamp time -> fmtRfc822Time time
-                                               AbsoluteExpires time -> fmtTimeEpochSeconds time]
-                                          , [] -- canonicalized AMZ headers
-                                          , [canonicalizedResource]]
--}
+
+import           Aws.Http
+import           Aws.Query
+import           Aws.Signature
+import           Aws.Util
+import           Data.Maybe
+import qualified Data.ByteString as B
+
+s3SignQuery :: () -> () -> SignatureData -> SignedQuery
+s3SignQuery x si sd = undefined
+    where
+      method = undefined
+      contentMd5 = Nothing
+      contentType = Nothing
+      canonicalizedResource = undefined
+      ti = signatureTimeInfo sd
+      stringToSign = B.intercalate "\n" $ concat [[httpMethod method]
+                                                 , [fromMaybe "" contentMd5]
+                                                 , [fromMaybe "" contentType]
+                                                 , [case ti of
+                                                      AbsoluteTimestamp time -> fmtRfc822Time time
+                                                      AbsoluteExpires time -> fmtTimeEpochSeconds time]
+                                                 , [] -- canonicalized AMZ headers
+                                                 , [canonicalizedResource]]
 
 {-
 instance AsQuery GetService where
