2aa9988273fb84ee1ff6a34fc8c570220183c5b4
Aristid Breitkreuz <aristidb@googlemail.com>
2011-02-20 20:52:26 +0100

    add S3 response stub

---
+++ Aws/S3/Response.hs
@@ -0,0 +1,10 @@
+module Aws.S3.Response
+where
+
+data S3Response a
+    = S3Response {
+        fromS3Response :: a
+      , s3AmzId2 :: String
+      , s3RequestId :: String
+      }
+    deriving (Show)
