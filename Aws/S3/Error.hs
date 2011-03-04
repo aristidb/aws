505bb0d0a2d2bb0f10af70b549d2c09b6ab71649
Aristid Breitkreuz <aristidb@googlemail.com>
2011-03-04 20:30:38 +0100

    add basic responseIteratee for S3 responses

--- Aws/S3/Error.hs
+++ Aws/S3/Error.hs
@@ -4,10 +4,9 @@ where
 
 import Data.Typeable
 import qualified Control.Exception as C
-  
+
 data S3Error
-    = S3MissingRequestId
-    | S3MissingAmz2Id
+    = S3UnknownError
     deriving (Show, Typeable)
 
 instance C.Exception S3Error
--- Aws/S3/Response.hs
+++ Aws/S3/Response.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
 module Aws.S3.Response
 where
+
+import           Aws.Response
+import           Aws.S3.Error
+import           Data.Maybe
+import qualified Data.ByteString.UTF8 as BU
+import qualified Data.Enumerator      as En
 
 data S3Response a
     = S3Response {
@@ -8,3 +15,14 @@ data S3Response a
       , s3RequestId :: String
       }
     deriving (Show)
+
+instance ResponseIteratee (S3Response ()) where
+    responseIteratee status headers = do
+      let headerString = fromMaybe "" . fmap BU.toString . flip lookup headers
+      let amzId2 = headerString "x-aamz-id-2"
+      let requestId = headerString "x-amz-request-id"
+      return $ S3Response {
+                   fromS3Response = ()
+                 , s3AmzId2 = amzId2
+                 , s3RequestId = requestId
+                 }
