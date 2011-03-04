e18959b79e76413a5562d40a9de15f2f73809874
Aristid Breitkreuz <aristidb@googlemail.com>
2011-03-05 00:46:08 +0100

    adapt Aws.S3.Error to accomodate XML errors

--- Aws/S3/Error.hs
+++ Aws/S3/Error.hs
@@ -2,11 +2,24 @@
 module Aws.S3.Error
 where
 
-import Data.Typeable
-import qualified Control.Exception as C
+import           Aws.Metadata
+import           Data.Typeable
+import           Text.XML.Monad
+import           Control.Monad.Error.Class
+import qualified Control.Exception         as C
 
 data S3Error
-    = S3UnknownError
+    = S3XmlError { 
+        fromS3XmlError :: XmlError
+      , s3XmlErrorMetadata :: Metadata
+      }
     deriving (Show, Typeable)
 
 instance C.Exception S3Error
+
+instance FromXmlError S3Error where
+    fromXmlError = flip S3XmlError NoMetadata
+
+instance Error S3Error where
+    noMsg = fromXmlError noMsg
+    strMsg = fromXmlError . strMsg
