bb56a7f94ec93f5339df835ff2b3474c374813f3
Aristid Breitkreuz <aristidb@googlemail.com>
2011-03-05 15:14:52 +0100

    type signatures on the LocationConstraints

--- Aws/S3/Model.hs
+++ Aws/S3/Model.hs
@@ -26,8 +26,9 @@ data BucketInfo
 
 type LocationConstraint = String
 
+locationUsClassic, locationUsWest, locationEu, locationApSouthEast, locationApNorthEast :: LocationConstraint
 locationUsClassic = ""
 locationUsWest = "us-west-1"
-locationEU = "EU"
+locationEu = "EU"
 locationApSouthEast = "ap-southeast-1"
 locationApNorthEast = "ap-northeast-1"
