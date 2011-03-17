9e3070ff9f0b474e1f57822b98014e5dae62c1bf
Aristid Breitkreuz <aristidb@googlemail.com>
2011-03-18 00:19:34 +0100

    S3: handle errors in s3ResponseIteratee and add Metadata

--- Aws/S3/Response.hs
+++ Aws/S3/Response.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances, OverloadedStrings, ScopedTypeVariables #-}
 module Aws.S3.Response
 where
 
+import           Aws.Metadata
 import           Aws.Response
+import           Aws.S3.Error
+import           Aws.Util
+import           Control.Applicative
+import           Control.Exception
 import           Data.Maybe
 import qualified Data.Ascii              as A
 import qualified Data.ByteString         as B
@@ -20,15 +25,20 @@ data S3Response a
 
 instance (S3ResponseIteratee a) => ResponseIteratee (S3Response a) where
     responseIteratee status headers = do
-      specific <- s3ResponseIteratee status headers
       let headerString = fromMaybe "" . fmap A.toString . flip lookup headers
       let amzId2 = headerString "x-amz-id-2"
       let requestId = headerString "x-amz-request-id"
-      return S3Response {
-                   fromS3Response = specific
-                 , s3AmzId2 = amzId2
-                 , s3RequestId = requestId
-                 }
+      
+      specific <- tryError $ s3ResponseIteratee status headers
+      
+      case specific of
+        Left (err :: S3Error) -> En.throwError (setMetadata' m err)
+            where m = S3Metadata { s3MAmzId2 = amzId2, s3MRequestId = requestId }
+        Right resp -> return S3Response {
+                                        fromS3Response = resp
+                                      , s3AmzId2 = amzId2
+                                      , s3RequestId = requestId
+                                      }
 
 class S3ResponseIteratee a where
     s3ResponseIteratee :: HTTP.Status -> HTTP.ResponseHeaders -> En.Iteratee B.ByteString IO a
--- Aws/Util.hs
+++ Aws/Util.hs
@@ -4,11 +4,19 @@ module Aws.Util
 where
   
 import           Control.Arrow
+import           Control.Exception
 import           Data.Time
 import           System.Locale
 import qualified Data.Ascii           as A
 import qualified Data.ByteString      as B
 import qualified Data.ByteString.UTF8 as BU
+import qualified Data.Enumerator      as En
+
+tryError :: (Exception e, Monad m) => En.Iteratee a m b -> En.Iteratee a m (Either e b)
+tryError m = En.catchError (fmap Right m) h
+    where h e = case fromException e of
+                  Just v -> return $ Left v
+                  Nothing -> En.throwError e
 
 queryList :: (a -> [(B.ByteString, B.ByteString)]) -> B.ByteString -> [a] -> [(B.ByteString, B.ByteString)]
 queryList f prefix xs = concat $ zipWith combine prefixList (map f xs)
