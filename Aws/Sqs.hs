04375019d397baf3972f93a9223a449b254ec710
Steve Severance <steve@medwizard.net>
2011-06-29 14:31:59 -0400

    rename done

---
+++ Aws/Sqs.hs
@@ -0,0 +1,17 @@
+module Aws.Sqs
+(
+  module Aws.Sqs.Commands
+, module Aws.Sqs.Error
+, module Aws.Sqs.Info
+, module Aws.Sqs.Metadata
+, module Aws.Sqs.Query
+, module Aws.Sqs.Response
+)
+where
+
+import Aws.Sqs.Commands
+import Aws.Sqs.Error
+import Aws.Sqs.Info
+import Aws.Sqs.Metadata
+import Aws.Sqs.Query
+import Aws.Sqs.Response
