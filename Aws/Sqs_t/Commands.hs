bff47fc51bb961c87989f751ee87e7455bb67823
Steve Severance <steve@medwizard.net>
2011-06-29 14:37:23 -0400

    renaming directory done

--- Aws/Sqs_t/Commands.hs
+++
@@ -1,6 +0,0 @@
-module Aws.SQS.Commands(
-  module Aws.SQS.Commands.ListQueues,
-  module Aws.SQS.Commands.GetQueueAttributes
-)where
-import Aws.SQS.Commands.ListQueues
-import Aws.SQS.Commands.GetQueueAttributes
--- Aws/Sqs_t/Metadata.hs
+++
@@ -1,9 +0,0 @@
-module Aws.SQS.Metadata
-where
-
-data SqsMetadata
-    = SqsMetadata {
-        sqsMAmzId2 :: String
-      , sqsMRequestId :: String
-      }
-    deriving (Show)
