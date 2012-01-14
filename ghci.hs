2c4ed50dbfdf591d897410b8c279987f35b4f4c1
Aristid Breitkreuz <aristidb@googlemail.com>
2012-01-14 22:40:41 +0100

    add SES to ghci.hs

--- ghci.hs
+++ ghci.hs
@@ -1,6 +1,7 @@
-import qualified Network.HTTP.Types      as HTTP
---import qualified Network.HTTP.Enumerator as HTTP
+import qualified Network.HTTP.Types   as HTTP
+import qualified Network.HTTP.Conduit as HTTP
 import           Aws
-import qualified Aws.S3                  as S3
-import qualified Aws.SimpleDb            as Sdb
-import qualified Aws.Sqs                 as Sqs
+import qualified Aws.S3               as S3
+import qualified Aws.SimpleDb         as Sdb
+import qualified Aws.Sqs              as Sqs
+import qualified Aws.Ses              as Ses
