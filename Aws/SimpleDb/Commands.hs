077e1614b2de73d890e7b763e7a7ca4f1ef0d401
Aristid Breitkreuz <aristidb@googlemail.com>
2012-05-01 23:39:06 +0200

    put SimpleDb commands into groups

--- Aws/SimpleDb/Commands.hs
+++ Aws/SimpleDb/Commands.hs
@@ -1,25 +1,11 @@
 module Aws.SimpleDb.Commands
 (
-  module Aws.SimpleDb.Commands.BatchDeleteAttributes
-, module Aws.SimpleDb.Commands.BatchPutAttributes
-, module Aws.SimpleDb.Commands.CreateDomain
-, module Aws.SimpleDb.Commands.DeleteAttributes
-, module Aws.SimpleDb.Commands.DeleteDomain
-, module Aws.SimpleDb.Commands.DomainMetadata
-, module Aws.SimpleDb.Commands.GetAttributes
-, module Aws.SimpleDb.Commands.ListDomains
-, module Aws.SimpleDb.Commands.PutAttributes
+  module Aws.SimpleDb.Commands.Attributes
+, module Aws.SimpleDb.Commands.Domain
 , module Aws.SimpleDb.Commands.Select
 )
 where
 
-import Aws.SimpleDb.Commands.BatchDeleteAttributes
-import Aws.SimpleDb.Commands.BatchPutAttributes
-import Aws.SimpleDb.Commands.CreateDomain
-import Aws.SimpleDb.Commands.DeleteAttributes
-import Aws.SimpleDb.Commands.DeleteDomain
-import Aws.SimpleDb.Commands.DomainMetadata
-import Aws.SimpleDb.Commands.GetAttributes
-import Aws.SimpleDb.Commands.ListDomains
-import Aws.SimpleDb.Commands.PutAttributes
+import Aws.SimpleDb.Commands.Attributes
+import Aws.SimpleDb.Commands.Domain
 import Aws.SimpleDb.Commands.Select
---
+++ Aws/SimpleDb/Commands/Attributes.hs
@@ -0,0 +1,178 @@
+{-# LANGUAGE RecordWildCards, TypeFamilies, FlexibleInstances, MultiParamTypeClasses, OverloadedStrings, TupleSections #-}
+module Aws.SimpleDb.Commands.Attributes where
+
+import           Aws.Core
+import           Aws.SimpleDb.Info
+import           Aws.SimpleDb.Metadata
+import           Aws.SimpleDb.Model
+import           Aws.SimpleDb.Query
+import           Aws.SimpleDb.Response
+import           Control.Applicative
+import           Control.Monad
+import           Data.Maybe
+import           Text.XML.Cursor            (($//), (&|))
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
+import qualified Text.XML.Cursor            as Cu
+
+data GetAttributes
+    = GetAttributes {
+        gaItemName :: T.Text
+      , gaAttributeName :: Maybe T.Text
+      , gaConsistentRead :: Bool
+      , gaDomainName :: T.Text
+      }
+    deriving (Show)
+
+data GetAttributesResponse
+    = GetAttributesResponse {
+        garAttributes :: [Attribute T.Text]
+      }
+    deriving (Show)
+
+getAttributes :: T.Text -> T.Text -> GetAttributes
+getAttributes item domain = GetAttributes { gaItemName = item, gaAttributeName = Nothing, gaConsistentRead = False, gaDomainName = domain }
+
+instance SignQuery GetAttributes where
+    type Info GetAttributes = SdbInfo
+    signQuery GetAttributes{..}
+        = sdbSignQuery $
+            [("Action", "GetAttributes"), ("ItemName", T.encodeUtf8 gaItemName), ("DomainName", T.encodeUtf8 gaDomainName)] ++
+            maybeToList (("AttributeName",) <$> T.encodeUtf8 <$> gaAttributeName) ++
+            (guard gaConsistentRead >> [("ConsistentRead", awsTrue)])
+
+instance ResponseConsumer r GetAttributesResponse where
+    type ResponseMetadata GetAttributesResponse = SdbMetadata
+    responseConsumer _ = sdbResponseConsumer parse
+        where parse cursor = do
+                sdbCheckResponseType () "GetAttributesResponse" cursor
+                attributes <- sequence $ cursor $// Cu.laxElement "Attribute" &| readAttribute
+                return $ GetAttributesResponse attributes
+
+instance Transaction GetAttributes GetAttributesResponse
+
+data PutAttributes
+    = PutAttributes {
+        paItemName :: T.Text
+      , paAttributes :: [Attribute SetAttribute]
+      , paExpected :: [Attribute ExpectedAttribute]
+      , paDomainName :: T.Text
+      }
+    deriving (Show)
+
+data PutAttributesResponse
+    = PutAttributesResponse
+    deriving (Show)
+             
+putAttributes :: T.Text -> [Attribute SetAttribute] -> T.Text -> PutAttributes
+putAttributes item attributes domain = PutAttributes { 
+                                         paItemName = item
+                                       , paAttributes = attributes
+                                       , paExpected = []
+                                       , paDomainName = domain 
+                                       }
+                                       
+instance SignQuery PutAttributes where
+    type Info PutAttributes = SdbInfo
+    signQuery PutAttributes{..}
+        = sdbSignQuery $ 
+            [("Action", "PutAttributes"), ("ItemName", T.encodeUtf8 paItemName), ("DomainName", T.encodeUtf8 paDomainName)] ++
+            queryList (attributeQuery setAttributeQuery) "Attribute" paAttributes ++
+            queryList (attributeQuery expectedAttributeQuery) "Expected" paExpected
+
+instance ResponseConsumer r PutAttributesResponse where
+    type ResponseMetadata PutAttributesResponse = SdbMetadata
+    responseConsumer _ = sdbResponseConsumer $ sdbCheckResponseType PutAttributesResponse "PutAttributesResponse"
+
+instance Transaction PutAttributes PutAttributesResponse
+
+data DeleteAttributes
+    = DeleteAttributes {
+        daItemName :: T.Text
+      , daAttributes :: [Attribute DeleteAttribute]
+      , daExpected :: [Attribute ExpectedAttribute]
+      , daDomainName :: T.Text
+      }
+    deriving (Show)
+
+data DeleteAttributesResponse
+    = DeleteAttributesResponse
+    deriving (Show)
+             
+deleteAttributes :: T.Text -> [Attribute DeleteAttribute] -> T.Text -> DeleteAttributes
+deleteAttributes item attributes domain = DeleteAttributes { 
+                                         daItemName = item
+                                       , daAttributes = attributes
+                                       , daExpected = []
+                                       , daDomainName = domain 
+                                       }
+                                       
+instance SignQuery DeleteAttributes where
+    type Info DeleteAttributes = SdbInfo
+    signQuery DeleteAttributes{..}
+        = sdbSignQuery $ 
+            [("Action", "DeleteAttributes"), ("ItemName", T.encodeUtf8 daItemName), ("DomainName", T.encodeUtf8 daDomainName)] ++
+            queryList (attributeQuery deleteAttributeQuery) "Attribute" daAttributes ++
+            queryList (attributeQuery expectedAttributeQuery) "Expected" daExpected
+
+instance ResponseConsumer r DeleteAttributesResponse where
+    type ResponseMetadata DeleteAttributesResponse = SdbMetadata
+    responseConsumer _ = sdbResponseConsumer $ sdbCheckResponseType DeleteAttributesResponse "DeleteAttributesResponse"
+
+instance Transaction DeleteAttributes DeleteAttributesResponse
+
+data BatchPutAttributes
+    = BatchPutAttributes {
+        bpaItems :: [Item [Attribute SetAttribute]]
+      , bpaDomainName :: T.Text
+      }
+    deriving (Show)
+
+data BatchPutAttributesResponse
+    = BatchPutAttributesResponse
+    deriving (Show)
+             
+batchPutAttributes :: [Item [Attribute SetAttribute]] -> T.Text -> BatchPutAttributes
+batchPutAttributes items domain = BatchPutAttributes { bpaItems = items, bpaDomainName = domain }
+
+instance SignQuery BatchPutAttributes where
+    type Info BatchPutAttributes = SdbInfo
+    signQuery BatchPutAttributes{..}
+        = sdbSignQuery $ 
+            [("Action", "BatchPutAttributes")
+            , ("DomainName", T.encodeUtf8 bpaDomainName)] ++
+            queryList (itemQuery $ queryList (attributeQuery setAttributeQuery) "Attribute") "Item" bpaItems
+
+instance ResponseConsumer r BatchPutAttributesResponse where
+    type ResponseMetadata BatchPutAttributesResponse = SdbMetadata
+    responseConsumer _ = sdbResponseConsumer $ sdbCheckResponseType BatchPutAttributesResponse "BatchPutAttributesResponse"
+
+instance Transaction BatchPutAttributes BatchPutAttributesResponse
+
+data BatchDeleteAttributes
+    = BatchDeleteAttributes {
+        bdaItems :: [Item [Attribute DeleteAttribute]]
+      , bdaDomainName :: T.Text
+      }
+    deriving (Show)
+
+data BatchDeleteAttributesResponse
+    = BatchDeleteAttributesResponse
+    deriving (Show)
+             
+batchDeleteAttributes :: [Item [Attribute DeleteAttribute]] -> T.Text -> BatchDeleteAttributes
+batchDeleteAttributes items domain = BatchDeleteAttributes { bdaItems = items, bdaDomainName = domain }
+
+instance SignQuery BatchDeleteAttributes where
+    type Info BatchDeleteAttributes = SdbInfo
+    signQuery BatchDeleteAttributes{..}
+        = sdbSignQuery $ 
+            [("Action", "BatchDeleteAttributes")
+            , ("DomainName", T.encodeUtf8 bdaDomainName)] ++
+            queryList (itemQuery $ queryList (attributeQuery deleteAttributeQuery) "Attribute") "Item" bdaItems
+
+instance ResponseConsumer r BatchDeleteAttributesResponse where
+    type ResponseMetadata BatchDeleteAttributesResponse = SdbMetadata
+    responseConsumer _ = sdbResponseConsumer $ sdbCheckResponseType BatchDeleteAttributesResponse "BatchDeleteAttributesResponse"
+
+instance Transaction BatchDeleteAttributes BatchDeleteAttributesResponse
