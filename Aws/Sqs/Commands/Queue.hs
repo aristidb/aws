0a50d4485a9514910a0a1160d463bff9ef95b4a2
Aristid Breitkreuz <aristidb@googlemail.com>
2012-05-02 00:57:54 +0200

    group Sqs commands

---
+++ Aws/Sqs/Commands/Queue.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE RecordWildCards, TypeFamilies, FlexibleInstances, MultiParamTypeClasses, OverloadedStrings, TupleSections #-}
+
+module Aws.Sqs.Commands.Queue where
+
+import           Aws.Core
+import           Aws.Sqs.Info
+import           Aws.Sqs.Metadata
+import           Aws.Sqs.Query
+import           Aws.Sqs.Response
+import           Aws.Sqs.Model
+import           Control.Applicative
+import           Data.Maybe
+import           Text.XML.Cursor       (($//), (&/))
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as TE
+import qualified Text.XML.Cursor       as Cu
+import qualified Data.ByteString.Char8 as B
+
+data CreateQueue = CreateQueue {
+    cqDefaultVisibilityTimeout :: Maybe Int,
+    cqQueueName :: T.Text
+  } deriving (Show)
+
+data CreateQueueResponse = CreateQueueResponse {
+    cqrQueueUrl :: T.Text
+  } deriving (Show)
+
+
+instance ResponseConsumer r CreateQueueResponse where
+    type ResponseMetadata CreateQueueResponse = SqsMetadata
+    responseConsumer _ = sqsXmlResponseConsumer parse
+      where
+        parse el = do
+          url <- force "Missing Queue Url" $ el $// Cu.laxElement "QueueUrl" &/ Cu.content
+          return CreateQueueResponse{ cqrQueueUrl = url}
+
+instance SignQuery CreateQueue  where
+    type Info CreateQueue  = SqsInfo
+    signQuery CreateQueue {..} = sqsSignQuery SqsQuery {
+                                             sqsQueueName = Nothing,
+                                             sqsQuery = [("Action", Just "CreateQueue"),
+                                                        ("QueueName", Just $ TE.encodeUtf8 cqQueueName)] ++
+                                                        catMaybes [("DefaultVisibilityTimeout",) <$> case cqDefaultVisibilityTimeout of
+                                                                                                       Just x -> Just $ Just $ B.pack $ show x
+                                                                                                       Nothing -> Nothing]}
+
+instance Transaction CreateQueue CreateQueueResponse
+
+data DeleteQueue = DeleteQueue {
+    dqQueueName :: QueueName 
+  } deriving (Show)
+
+data DeleteQueueResponse = DeleteQueueResponse 
+  deriving (Show)
+
+instance ResponseConsumer r DeleteQueueResponse where
+    type ResponseMetadata DeleteQueueResponse = SqsMetadata
+    responseConsumer _ = sqsXmlResponseConsumer parse
+      where
+        parse _ = do return DeleteQueueResponse{}
+          
+instance SignQuery DeleteQueue  where 
+    type Info DeleteQueue  = SqsInfo
+    signQuery DeleteQueue {..} = sqsSignQuery SqsQuery {
+                                             sqsQueueName = Just dqQueueName, 
+                                             sqsQuery = [("Action", Just "DeleteQueue")]}
+
+instance Transaction DeleteQueue DeleteQueueResponse
+
+data ListQueues = ListQueues {
+    lqQueueNamePrefix :: Maybe T.Text
+  } deriving (Show)
+
+data ListQueuesResponse = ListQueuesResponse {
+    lqrQueueUrls :: [T.Text]
+  } deriving (Show)
+
+instance ResponseConsumer r ListQueuesResponse where
+    type ResponseMetadata ListQueuesResponse = SqsMetadata
+    responseConsumer _ = sqsXmlResponseConsumer parse
+      where
+        parse el = do
+            let queues = el $// Cu.laxElement "QueueUrl" &/ Cu.content
+            return ListQueuesResponse { lqrQueueUrls = queues }
+
+instance SignQuery ListQueues where
+    type Info ListQueues = SqsInfo
+    signQuery ListQueues{..} = sqsSignQuery SqsQuery {
+                                              sqsQueueName = Nothing,
+                                              sqsQuery = [("Action", Just "ListQueues")] ++ catMaybes [
+                                              ("QueueNamePrefix",) <$> case lqQueueNamePrefix of
+                                                                         Just x  -> Just $ Just $ TE.encodeUtf8 x
+                                                                         Nothing -> Nothing]}
+
+instance Transaction ListQueues ListQueuesResponse
--- Aws/Sqs/Commands/QueueAttributes.hs
+++ Aws/Sqs/Commands/QueueAttributes.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE RecordWildCards, TypeFamilies, FlexibleInstances, MultiParamTypeClasses, OverloadedStrings, TupleSections #-}
 
-module Aws.Sqs.Commands.GetQueueAttributes where
+module Aws.Sqs.Commands.QueueAttributes where
 
 import           Aws.Core
 import           Aws.Sqs.Info
 import           Aws.Sqs.Metadata
-import qualified Aws.Sqs.Model as M
 import           Aws.Sqs.Query
 import           Aws.Sqs.Response
-import           Text.XML.Cursor              (($/), ($//), (&/), (&|))
-import qualified Data.Text                    as T
-import qualified Text.XML.Cursor              as Cu
+import           Text.XML.Cursor       (($/), ($//), (&/), (&|))
+import qualified Aws.Sqs.Model         as M
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as TE
+import qualified Text.XML.Cursor       as Cu
 
 data GetQueueAttributes = GetQueueAttributes {
   gqaQueueName :: M.QueueName,
@@ -51,3 +52,29 @@ instance SignQuery GetQueueAttributes where
                                               sqsQuery = [("Action", Just "GetQueueAttributes")] ++ (formatAttributes gqaAttributes)}
 
 instance Transaction GetQueueAttributes GetQueueAttributesResponse
+
+data SetQueueAttributes = SetQueueAttributes{
+  sqaAttribute :: M.QueueAttribute,
+  sqaValue :: T.Text,
+  sqaQueueName :: M.QueueName 
+}deriving (Show)
+
+data SetQueueAttributesResponse = SetQueueAttributesResponse{
+} deriving (Show)
+
+instance ResponseConsumer r SetQueueAttributesResponse where
+    type ResponseMetadata SetQueueAttributesResponse = SqsMetadata
+    responseConsumer _ = sqsXmlResponseConsumer parse
+      where 
+        parse _ = do
+          return SetQueueAttributesResponse {}
+          
+instance SignQuery SetQueueAttributes  where 
+    type Info SetQueueAttributes  = SqsInfo
+    signQuery SetQueueAttributes {..} = sqsSignQuery SqsQuery { 
+                                             sqsQueueName = Just sqaQueueName,
+                                             sqsQuery = [("Action", Just "SetQueueAttributes"), 
+                                                        ("Attribute.Name", Just $ TE.encodeUtf8 $ M.printQueueAttribute sqaAttribute),
+                                                        ("Attribute.Value", Just $ TE.encodeUtf8 sqaValue)]} 
+
+instance Transaction SetQueueAttributes SetQueueAttributesResponse
