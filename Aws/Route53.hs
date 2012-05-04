953284f24f447a2e2a2ba00e52b7f319fca34148
Lars Kuhtz <lars@kuhtz.eu>
2012-05-03 23:13:36 -0700

    Route53: fix Aws.Route53 to not export the modules of the old scheme

--- Aws/Route53.hs
+++ Aws/Route53.hs
@@ -1,18 +1,8 @@
 module Aws.Route53
 ( module Aws.Route53.Commands
-, module Aws.Route53.Error
-, module Aws.Route53.Info
-, module Aws.Route53.Metadata
-, module Aws.Route53.Model
-, module Aws.Route53.Query
-, module Aws.Route53.Response
+, module Aws.Route53.Core
 )
 where
 
 import Aws.Route53.Commands
-import Aws.Route53.Error
-import Aws.Route53.Info
-import Aws.Route53.Metadata
-import Aws.Route53.Model
-import Aws.Route53.Query
-import Aws.Route53.Response
+import Aws.Route53.Core
