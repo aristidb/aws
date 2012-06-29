2405a23730fd8f966196bd9a9b4031cc3355146d
Aristid Breitkreuz <aristidb@googlemail.com>
2012-06-29 04:27:26 +0200

    fix argument order to S3.putObject

--- Aws/S3/Commands/PutObject.hs
+++ Aws/S3/Commands/PutObject.hs
@@ -30,8 +30,8 @@ data PutObject = PutObject {
   poMetadata :: [(T.Text,T.Text)]
 }
 
-putObject :: T.Text -> Bucket -> HTTP.RequestBody (C.ResourceT IO) -> PutObject
-putObject obj bucket body = PutObject obj bucket Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing body []
+putObject :: Bucket -> T.Text -> HTTP.RequestBody (C.ResourceT IO) -> PutObject
+putObject bucket obj body = PutObject obj bucket Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing body []
 
 data PutObjectResponse 
   = PutObjectResponse {
