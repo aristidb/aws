c10e6aedc70fe82b5b77751017304698d9f83d5c
Aristid Breitkreuz <aristidb@googlemail.com>
2012-08-04 14:13:07 +0200

    ad new function to properly parse HTTP-dates, which should work in older versions of the time library as well as actually be correct (related to #28)

--- Aws/S3/Core.hs
+++ Aws/S3/Core.hs
@@ -372,7 +372,7 @@ parseObjectMetadata h = ObjectMetadata
                  Just x -> return x
                  Nothing -> F.failure $ HeaderException "ETag missing"
         lastModified = case B8.unpack `fmap` lookup "Last-Modified" h of
-                         Just ts -> case parseTime defaultTimeLocale rfc822Time ts of
+                         Just ts -> case parseHttpDate ts of
                                       Just t -> return t
                                       Nothing -> F.failure $ HeaderException "Invalid Last-Modified"
                          Nothing -> F.failure $ HeaderException "Last-Modified missing"
