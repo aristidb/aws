f7997f2f2d692c851a9c123d98623508d3edecfd
Aristid Breitkreuz <aristidb@googlemail.com>
2012-08-14 14:59:08 +0200

    more convenience for ghci.hs

--- ghci.hs
+++ ghci.hs
@@ -5,9 +5,16 @@ import qualified Aws.S3 as S3
 import qualified Aws.Ses as Ses
 import qualified Aws.SimpleDb as Sdb
 import qualified Aws.Sqs as Sqs
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 import           Data.Default
 import qualified Network.HTTP.Conduit as HTTP
 import qualified Network.HTTP.Types as HTTP
+
+import           System.IO.Unsafe -- only for the initialisation Please
+
+bcfg = unsafePerformIO baseConfiguration
+dcfg = unsafePerformIO dbgConfiguration
+mgr = unsafePerformIO (HTTP.newManager def)
