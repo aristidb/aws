1ac2e86a04afdec363ba19194347c2bee5bb18b2
Aristid Breitkreuz <aristidb@googlemail.com>
2012-08-24 01:44:50 +0200

    simplify GetObject example

--- Examples/GetObject.hs
+++ Examples/GetObject.hs
@@ -4,8 +4,6 @@ import qualified Aws
 import qualified Aws.S3 as S3
 import           Data.Conduit (($$+-))
 import           Data.Conduit.Binary (sinkFile)
-import           Data.IORef (newIORef, readIORef)
-import           Data.Monoid (mempty)
 import           Network.HTTP.Conduit (withManager, responseBody)
 
 main :: IO ()
@@ -13,18 +11,12 @@ main = do
   {- Set up AWS credentials and the default configuration. -}
   cfg <- Aws.baseConfiguration
 
-  {- Create an IORef to store the response Metadata (so it is also available in case of an error). -}
-  metadataRef <- newIORef mempty
-
   {- Set up a ResourceT region with an available HTTP manager. -}
   withManager $ \mgr -> do
-    {- Create a request object with S3.getObject and run the request with awsRef. -}
+    {- Create a request object with S3.getObject and run the request with pureAws. -}
     S3.GetObjectResponse { S3.gorResponse = rsp } <-
-      Aws.awsRef cfg Aws.defServiceConfig mgr metadataRef $
+      Aws.pureAws cfg Aws.defServiceConfig mgr $
         S3.getObject "haskell-aws" "cloud-remote.pdf"
 
     {- Save the response to a file. -}
     responseBody rsp $$+- sinkFile "cloud-remote.pdf"
-
-  {- Print the response metadata. -}
-  print =<< readIORef metadataRef
