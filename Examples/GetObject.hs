3474c0126e65443d93ecec59706123d1e524fc6e
Aristid Breitkreuz <aristidb@googlemail.com>
2012-08-23 23:19:27 +0200

    fix GetObject example (not yet in README)

--- Examples/GetObject.hs
+++ Examples/GetObject.hs
@@ -2,26 +2,29 @@
 
 import qualified Aws
 import qualified Aws.S3 as S3
-import Data.Conduit
-import Data.Conduit.Binary
-import Data.IORef
-import Data.Monoid
-
-{- A small function to save the object's data into a file. -}
-saveObject :: Aws.HTTPResponseConsumer ()
-saveObject status headers source = source $$+- sinkFile "cloud-remote.pdf"
+import           Data.Conduit (($$+-))
+import           Data.Conduit.Binary (sinkFile)
+import           Data.IORef (newIORef, readIORef)
+import           Data.Monoid (mempty)
+import           Network.HTTP.Conduit (withManager, responseBody)
 
 main :: IO ()
 main = do
   {- Set up AWS credentials and the default configuration. -}
   cfg <- Aws.baseConfiguration
-  let s3Cfg = Aws.defServiceConfig
 
   {- Create an IORef to store the response Metadata (so it is also available in case of an error). -}
   metadataRef <- newIORef mempty
 
-  {- Create a request object with S3.getObject and run the request with simpleAwsRef. -}
-  Aws.simpleAwsRef cfg s3Cfg metadataRef $ S3.getObject "haskell-aws" "cloud-remote.pdf" saveObject
+  {- Set up a ResourceT region with an available HTTP manager. -}
+  withManager $ \mgr -> do
+    {- Create a request object with S3.getObject and run the request with awsRef. -}
+    S3.GetObjectResponse { S3.gorResponse = rsp } <-
+      Aws.awsRef cfg Aws.defServiceConfig mgr metadataRef $
+        S3.getObject "haskell-aws" "cloud-remote.pdf"
+
+    {- Save the response to a file. -}
+    responseBody rsp $$+- sinkFile "cloud-remote.pdf"
 
   {- Print the response metadata. -}
   print =<< readIORef metadataRef
