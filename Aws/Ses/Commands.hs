e1c9c104a78e12e35f9f202cdb686f22fac11aec
Connor Williams <connor@janrain.com>
2013-04-03 11:19:32 -0700

    support SES VerifyEmailIdentity

--- Aws/Ses/Commands.hs
+++ Aws/Ses/Commands.hs
@@ -1,7 +1,9 @@
 module Aws.Ses.Commands
     ( module Aws.Ses.Commands.SendRawEmail
     , module Aws.Ses.Commands.ListIdentities
+    , module Aws.Ses.Commands.VerifyEmailIdentity
     ) where
 
 import Aws.Ses.Commands.SendRawEmail
 import Aws.Ses.Commands.ListIdentities
+import Aws.Ses.Commands.VerifyEmailIdentity
