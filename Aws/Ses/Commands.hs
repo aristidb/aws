b8a6eb82622005a900212b892a31da3b661ad035
Abhinav Gupta <mail@abhinavg.net>
2013-05-20 23:33:02 -0400

    Support SES VerifyDomainIdentity.

--- Aws/Ses/Commands.hs
+++ Aws/Ses/Commands.hs
@@ -2,6 +2,7 @@ module Aws.Ses.Commands
     ( module Aws.Ses.Commands.SendRawEmail
     , module Aws.Ses.Commands.ListIdentities
     , module Aws.Ses.Commands.VerifyEmailIdentity
+    , module Aws.Ses.Commands.VerifyDomainIdentity
     , module Aws.Ses.Commands.DeleteIdentity
     , module Aws.Ses.Commands.GetIdentityVerificationAttributes
     ) where
@@ -9,5 +10,6 @@ module Aws.Ses.Commands
 import Aws.Ses.Commands.SendRawEmail
 import Aws.Ses.Commands.ListIdentities
 import Aws.Ses.Commands.VerifyEmailIdentity
+import Aws.Ses.Commands.VerifyDomainIdentity
 import Aws.Ses.Commands.DeleteIdentity
 import Aws.Ses.Commands.GetIdentityVerificationAttributes
