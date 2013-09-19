68478c6183841e9329f654918b6bc4d87ab3f5f4
tdp93422 <tim.v2.0@gmail.com>
2013-09-19 11:43:34 -0700

    simplified example to remove new dependencies for package

--- Examples/Sqs.hs
+++ Examples/Sqs.hs
@@ -3,16 +3,10 @@
 import qualified Aws
 import qualified Aws.Core
 import qualified Aws.Sqs as Sqs
-import           Data.Conduit (($$+-))
-import           Data.Conduit.Binary (sinkFile)
-import           Network.HTTP.Conduit (withManager, responseBody)
 import qualified Data.Text.IO as T
 import qualified Data.Text    as T
 import qualified Data.Text.Read as TR
 import Control.Monad (forM_, forM, replicateM)
-
-import qualified Data.Set as Set
-import Data.Time.Clock (diffUTCTime, getCurrentTime)
 
 {-| Created by Tim Perry on September 18, 2013 
   |
@@ -56,22 +50,16 @@ main = do
   mapM_ (\(attName, attText) -> T.putStrLn $ T.concat ["     ", Sqs.printQueueAttribute attName, " ", attText]) attPairs
 
   {- Here we add some messages to the queue -}
-  msgAddStartTime  <- getCurrentTime
-  let messages = Set.fromList $ map (\n -> T.pack $ "msg" ++ (show n)) [1 .. 10]
+  let messages = map (\n -> T.pack $ "msg" ++ (show n)) [1 .. 10]
   {- Add messages to the queue -}
-  forM_ (Set.elems messages) (\mText -> do
+  forM_ messages (\mText -> do
       T.putStrLn $ T.concat ["   Adding: ", mText]
       let sqsSendMessage = Sqs.SendMessage mText sqsQName
       Aws.simpleAws cfg sqscfg sqsSendMessage)
 
-  {- check to see if AWS is acknowledging all the messages yet. 
-  |    This is *silly* -- it is essentially a spin lock -}
-  putStrLn $ "Check to see if the " ++ (show $ Set.size messages) ++ " messages showed up:"
-  checkForAllMessages cfg sqscfg msgAddStartTime sqsQName (Set.size messages)
-
   {- Here we remove messages from the queue one at a time. -}
   let receiveMessageReq = Sqs.ReceiveMessage Nothing [] (Just 1) sqsQName
-  numMessages <- qMsgCount cfg sqscfg sqsQName
+  let numMessages = length messages
   removedMsgs <- replicateM numMessages $ do
       Sqs.ReceiveMessageResponse msgs <- Aws.simpleAws cfg sqscfg receiveMessageReq
       forM msgs (\msg -> do
@@ -80,13 +68,6 @@ main = do
                      putStrLn $ "Received " ++ (show $ Sqs.mBody msg)
                      Aws.simpleAws cfg sqscfg $ Sqs.DeleteMessage (Sqs.mReceiptHandle msg) sqsQName
                      return $ Sqs.mBody msg)
-
-  let otherMessages = foldl (\set target -> Set.delete target set) messages (concat removedMsgs)
-  if (0 == Set.size otherMessages)
-      then putStrLn "all sent messages were received and deleted"
-      else do
-         putStrLn "not all sent messages were received"
-         forM_ (Set.toList otherMessages) (\m -> putStrLn ("    " ++ (show m) ++ " was not deleted"))
 
   {- Now we'll delete the queue we created at the start of this program -}
   putStrLn $ "Deleting the queue: " ++ (show $ Sqs.qName sqsQName)
@@ -101,34 +82,7 @@ main = do
   mapM_ T.putStrLn qUrls
 
   if qUrl `elem` qUrls
-     then putStrLn $ " *\n *\n * Warning, '" ++ (show qName) ++ "' was not deleted"
+     then putStrLn $ " *\n *\n * Warning, '" ++ (show qName) ++ "' was not deleted\n"
+                  ++ " * This is probably just a race condition."
      else putStrLn $ "     The queue '" ++ (show qName) ++ "' was correctly deleted"
 
-{- A helper method which asks the Queue how many messages are available -}
-qMsgCount cfg sqscfg sqsQName = do
-  let qAttReq = Sqs.GetQueueAttributes sqsQName [Sqs.ApproximateNumberOfMessages]
-  Sqs.GetQueueAttributesResponse attPairs <- Aws.simpleAws cfg sqscfg qAttReq
-  let (Right (cnt, _)) = TR.decimal $ snd $ head attPairs 
-  return cnt
-        
-
-{- a helper method that keeps requerying the queue until all the messages sent to it show up -}
-checkForAllMessages cfg sqscfg msgAddStartTime sqsQName msgSentCount = do
-  let qAttReq = Sqs.GetQueueAttributes sqsQName [Sqs.ApproximateNumberOfMessages]
-  Sqs.GetQueueAttributesResponse attPairs <- Aws.simpleAws cfg sqscfg qAttReq
-  endTime <- getCurrentTime
-  let lapse =  T.pack $ (show (endTime `diffUTCTime` msgAddStartTime)) ++ " seconds"
-  mapM_ (\(_, attText) -> T.putStrLn $ T.concat ["     after ", lapse , " ", attText]) attPairs
-
-  let eitherCount = TR.decimal $ snd $ head attPairs 
-  case eitherCount of
-     Right (msgsAdded, _) -> do
-         if (msgsAdded >= msgSentCount)
-             then do
-                 putStrLn $ "All messages received after " ++ (show (endTime `diffUTCTime` msgAddStartTime)) ++ " seconds"
-             else do
-                 checkForAllMessages cfg sqscfg msgAddStartTime sqsQName msgSentCount
-     Left errMsg -> do 
-        putStrLn $ "Error parsing number of messages in queue:\n   " ++ errMsg
-        checkForAllMessages cfg sqscfg msgAddStartTime sqsQName msgSentCount
-         
