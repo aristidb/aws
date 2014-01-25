f2fccf85d81a54ec62c2a233ce270a60ededaa04
John Lenz <lenz@math.uic.edu>
2014-01-25 13:39:45 -0600

    Move V4 authorization into Aws.Core

--- Aws/DynamoDb/Core.hs
+++ Aws/DynamoDb/Core.hs
@@ -2,8 +2,7 @@ module Aws.DynamoDb.Core where
 
 import           Aws.Core
 import qualified Control.Exception              as C
-import           Crypto.HMAC (hmac', MacKey(..))
-import           Crypto.Hash.CryptoAPI (SHA256, hash, hash')
+import           Crypto.Hash.CryptoAPI (SHA256, hash)
 import qualified Data.Aeson                     as A
 import qualified Data.ByteString                as B
 import qualified Data.ByteString.Base16         as Base16
@@ -66,20 +65,6 @@ dyLocal = DyConfiguration HTTP "localhost" 8000 "local"
 dyApiVersion :: B.ByteString
 dyApiVersion = "DynamoDB_20120810."
 
--- | Create a signature for Signature Version 4
-signatureV4 :: DyConfiguration qt -> Credentials -> B.ByteString -> B.ByteString -> B.ByteString
-signatureV4 di cr sigDate input = Base16.encode sig
-    where
-      hmac256 :: B.ByteString -> B.ByteString -> B.ByteString
-      hmac256 k i = Serialize.encode (hmac' (MacKey k) i :: SHA256) 
-
-      kDate = hmac256 ("AWS4" <> secretAccessKey cr) sigDate
-      kRegion = hmac256 kDate $ dyRegion di
-      kService = hmac256 kRegion "dynamodb"
-      kSigning = hmac256 kService "aws4_request"
-
-      sig = hmac256 kSigning input
-
 dySignQuery :: A.ToJSON a => B.ByteString -> a -> DyConfiguration qt -> SignatureData -> SignedQuery
 dySignQuery target body di sd
     = SignedQuery {
@@ -90,7 +75,7 @@ dySignQuery target body di sd
       , sqPath = "/"
       , sqQuery = []
       , sqDate = Just $ signatureTime sd
-      , sqAuthorization = Just authorization
+      , sqAuthorization = Just auth
       , sqContentType = Just "application/x-amz-json-1.0"
       , sqContentMd5 = Nothing
       , sqAmzHeaders = [ ("X-Amz-Target", dyApiVersion <> target)
@@ -98,16 +83,13 @@ dySignQuery target body di sd
                        ]
       , sqOtherHeaders = []
       , sqBody = Just $ HTTP.RequestBodyLBS bodyLBS
-      , sqStringToSign = stringToSign
+      , sqStringToSign = canonicalRequest
       }
     where
         sigTime = fmtTime "%Y%m%dT%H%M%SZ" $ signatureTime sd
-        sigDate = fmtTime "%Y%m%d" $ signatureTime sd
 
         hash256 :: BL.ByteString -> SHA256
         hash256 = hash
-        hash256' :: B.ByteString -> SHA256
-        hash256' = hash'
 
         bodyLBS = A.encode body
         bodyHash = Base16.encode $ Serialize.encode $ hash256 bodyLBS
@@ -120,7 +102,7 @@ dySignQuery target body di sd
                                     , dyHost di
                                     , "\n"
                                     , "x-amz-date:"
-                                    , sigDate
+                                    , sigTime
                                     , "\n"
                                     , "x-amz-target:"
                                     , dyApiVersion
@@ -131,36 +113,17 @@ dySignQuery target body di sd
                                     , bodyHash
                                     ]
 
-        canonicalRequestHash = Base16.encode $ Serialize.encode $ hash256' canonicalRequest
-
-        stringToSign = B.concat [ "AWS4-HMAC-SHA256\n"
-                                , sigTime
-                                , "\n"
-                                , sigDate
-                                , "/"
-                                , dyRegion di
-                                , "/dynamodb/aws4_request\n"
-                                , canonicalRequestHash
-                                ]
-
-        sig = signatureV4 di (signatureCredentials sd) sigDate stringToSign
-
-        authorization = B.concat [ "AWS4-HMAC-SHA256 Credential="
-                                 , accessKeyID (signatureCredentials sd)
-                                 , "/"
-                                 , sigDate
-                                 , "/"
-                                 , dyRegion di
-                                 , "/dynamodb/aws4_request,"
-                                 , "SignedHeaders=content-type;host;x-amz-date;x-amz-target,"
-                                 , "Signature="
-                                 , sig
-                                 ]
+        auth = authorizationV4 sd HmacSHA256 (dyRegion di) "dynamodb"
+                               "content-type;host;x-amz-date;x-amz-target"
+                               canonicalRequest
 
 dyResponseConsumer :: A.FromJSON a
                    => HTTPResponseConsumer a
 dyResponseConsumer resp = do
     val <- HTTP.responseBody resp $$+- Atto.sinkParser A.json'
-    case A.fromJSON val of
-        A.Success a -> return a
-        A.Error err -> monadThrow $ DyError (HTTP.responseStatus resp) "" err
+    case HTTP.responseStatus resp of
+        (HTTP.Status{HTTP.statusCode=200}) -> do
+            case A.fromJSON val of
+                A.Success a -> return a
+                A.Error err -> monadThrow $ DyError (HTTP.responseStatus resp) "" err
+        _ -> monadThrow $ DyError (HTTP.responseStatus resp) "" (show val)
