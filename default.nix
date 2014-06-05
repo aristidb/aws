ecb4211d7c28c85a58f4a42351a8672aa10cde89
Aristid Breitkreuz <aristidb@gmail.com>
2014-06-05 21:25:23 +0200

    transformers 0.4

--- default.nix
+++ default.nix
@@ -1,2 +1,3 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.callPackage ./aws.nix {}
+let hask = pkgs.haskellPackages_ghc782.override { extension = self : super : { transformers = self.transformers_0_4_1_0; }; };
+in hask.callPackage ./aws.nix {}
