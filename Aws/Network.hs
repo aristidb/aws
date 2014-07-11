a0a4581dfb260c91d2341491e0325f2128b22dc6
Maxwell Swadling <maxwell.swadling@nicta.com.au>
2014-07-11 12:04:54 +1000

    fixed timeout

--- Aws/Network.hs
+++ Aws/Network.hs
@@ -1,20 +1,20 @@
 module Aws.Network where
 
+import Data.Maybe
 import Control.Exception
 import Network.BSD (getProtocolNumber)
 import Network.Socket
+import System.Timeout
 
 -- It may give false positives, but never false negatives.
 hostAvailable :: String -> IO Bool
 hostAvailable h = do
-  -- addrInfo <- (addrAddress . head) `fmap` getAddrInfo Nothing (Just h) Nothing
-  remote <- getProtocolNumber "icmp" >>= socket AF_INET Datagram
+  sock <- getProtocolNumber "tcp" >>= socket AF_INET Stream
   addr <- (addrAddress . head) `fmap` getAddrInfo (Just (defaultHints { addrFlags = [ AI_PASSIVE ] } )) (Just h) (Just "80")
   case addr of
-    (SockAddrInet _ s) -> do
-      let local = (SockAddrInet aNY_PORT s)
-      v <- catch (bindSocket remote local >> sendTo remote "" local >> return True)
+    remote@(SockAddrInet _ _) -> do
+      v <- catch (timeout 100000 (connect sock remote) >>= return . isJust)
                  (\(_ :: SomeException) -> return False)
-      sClose remote
+      sClose sock
       return v
-    _ -> return False -- disconnected
+    _ -> return False
