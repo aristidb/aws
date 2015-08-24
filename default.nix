720a76cd90dd9cd613305686d46bd3931b457f5e
James Thompson <james@orphid.com>
2015-08-24 11:24:10 -0700

    update nix config

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ mkDerivation, aeson, attoparsec, base, base16-bytestring
+, base64-bytestring, blaze-builder, byteable, bytestring
+, case-insensitive, cereal, conduit, conduit-extra, containers
+, cryptohash, data-default, directory, errors-1_4_7, filepath
+, http-client, http-conduit, http-types, lifted-base, monad-control
+, mtl, network, old-locale, QuickCheck, quickcheck-instances
+, resourcet, safe, scientific, stdenv, tagged, tasty
+, tasty-quickcheck, text, time, transformers, transformers-base
+, unordered-containers, utf8-string, vector, xml-conduit
+}:
+mkDerivation {
+  pname = "aws";
+  version = "0.12.2";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    aeson attoparsec base base16-bytestring base64-bytestring
+    blaze-builder byteable bytestring case-insensitive cereal conduit
+    conduit-extra containers cryptohash data-default directory filepath
+    http-conduit http-types lifted-base monad-control mtl network
+    old-locale resourcet safe scientific tagged text time transformers
+    unordered-containers utf8-string vector xml-conduit
+  ];
+  testDepends = [
+    aeson base bytestring errors-1_4_7 http-client lifted-base monad-control
+    mtl QuickCheck quickcheck-instances resourcet tagged tasty
+    tasty-quickcheck text time transformers transformers-base
+  ];
+  jailbreak = true;
+  homepage = "http://github.com/aristidb/aws";
+  description = "Amazon Web Services (AWS) for Haskell";
+  license = stdenv.lib.licenses.bsd3;
+}
