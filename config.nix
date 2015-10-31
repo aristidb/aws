08ecc29bea48497b7b856813178e237a0296ff0b
Aristid Breitkreuz <aristidb@gmail.com>
2015-10-31 13:11:01 +0100

    remove obsolete nix files

--- config.nix
+++
@@ -1,7 +0,0 @@
-{
-  packageOverrides = pkgs: rec {
-    haskellPackages = with pkgs.haskellPackages; pkgs.haskellPackages // rec {
-       errors  = callPackage ./errors.nix {};
-     };
-  };
-}
--- errors.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, either, safe, stdenv, transformers }:
-mkDerivation {
-  pname = "errors";
-  version = "1.4.7";
-  sha256 = "09g53dylwsw1phxq5zhkbq8pnpwqzipvqclmcrdypzkpwkmfncl7";
-  buildDepends = [ base either safe transformers ];
-  description = "Simplified error-handling";
-  license = stdenv.lib.licenses.bsd3;
-}
