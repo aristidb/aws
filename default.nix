c6f2ccf2b6a79161d1d224c709b70f0db30aaead
Aristid Breitkreuz <aristidb@gmail.com>
2016-03-31 19:55:53 +0200

    update default.nix

--- default.nix
+++ default.nix
@@ -2,11 +2,12 @@
 , base64-bytestring, blaze-builder, byteable, bytestring
 , case-insensitive, cereal, conduit, conduit-extra, containers
 , cryptohash, data-default, directory, errors, filepath
-, http-client, http-conduit, http-types, lifted-base, monad-control
-, mtl, network, old-locale, QuickCheck, quickcheck-instances
-, resourcet, safe, scientific, stdenv, tagged, tasty
-, tasty-quickcheck, text, time, transformers, transformers-base
-, unordered-containers, utf8-string, vector, xml-conduit
+, http-client, http-client-tls, http-conduit, http-types
+, lifted-base, monad-control, mtl, network, old-locale, QuickCheck
+, quickcheck-instances, resourcet, safe, scientific, stdenv, tagged
+, tasty, tasty-hunit, tasty-quickcheck, text, time, transformers
+, transformers-base, unordered-containers, utf8-string, vector
+, xml-conduit
 }:
 mkDerivation {
   pname = "aws";
@@ -23,9 +24,10 @@ mkDerivation {
     unordered-containers utf8-string vector xml-conduit
   ];
   testHaskellDepends = [
-    aeson base bytestring errors http-client lifted-base monad-control
-    mtl QuickCheck quickcheck-instances resourcet tagged tasty
-    tasty-quickcheck text time transformers transformers-base
+    aeson base bytestring errors http-client http-client-tls http-types
+    lifted-base monad-control mtl QuickCheck quickcheck-instances
+    resourcet tagged tasty tasty-hunit tasty-quickcheck text time
+    transformers transformers-base
   ];
   homepage = "http://github.com/aristidb/aws";
   description = "Amazon Web Services (AWS) for Haskell";
