1648e96d1e0ba9f6fca8864e840ad9fbc4fc8816
Aristid Breitkreuz <aristidb@gmail.com>
2017-09-30 17:32:47 +0200

    fix testsuite build

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
 { mkDerivation, aeson, attoparsec, base, base16-bytestring
 , base64-bytestring, blaze-builder, byteable, bytestring
-, case-insensitive, cereal, conduit, conduit-extra, containers
-, cryptohash, data-default, directory, errors, filepath
-, http-client, http-client-tls, http-conduit, http-types
-, lifted-base, monad-control, mtl, network, old-locale, QuickCheck
-, quickcheck-instances, resourcet, safe, scientific, stdenv, tagged
-, tasty, tasty-hunit, tasty-quickcheck, text, time, transformers
-, transformers-base, unordered-containers, utf8-string, vector
-, xml-conduit
+, case-insensitive, cereal, conduit, conduit-combinators
+, conduit-extra, containers, cryptohash, data-default, directory
+, errors, filepath, http-client, http-client-tls, http-conduit
+, http-types, lifted-base, monad-control, mtl, network, old-locale
+, QuickCheck, quickcheck-instances, resourcet, safe, scientific
+, stdenv, tagged, tasty, tasty-hunit, tasty-quickcheck, text, time
+, transformers, transformers-base, unordered-containers
+, utf8-string, vector, xml-conduit
 }:
 mkDerivation {
   pname = "aws";
-  version = "0.13.0";
+  version = "0.17";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -24,10 +24,10 @@ mkDerivation {
     unordered-containers utf8-string vector xml-conduit
   ];
   testHaskellDepends = [
-    aeson base bytestring errors http-client http-client-tls http-types
-    lifted-base monad-control mtl QuickCheck quickcheck-instances
-    resourcet tagged tasty tasty-hunit tasty-quickcheck text time
-    transformers transformers-base
+    aeson base bytestring conduit-combinators errors http-client
+    http-client-tls http-types lifted-base monad-control mtl QuickCheck
+    quickcheck-instances resourcet tagged tasty tasty-hunit
+    tasty-quickcheck text time transformers transformers-base
   ];
   homepage = "http://github.com/aristidb/aws";
   description = "Amazon Web Services (AWS) for Haskell";
